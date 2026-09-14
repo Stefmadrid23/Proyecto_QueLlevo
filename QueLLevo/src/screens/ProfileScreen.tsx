a0ad6ef387d47cdd624596554ae0b7b39a2ae75d
Stefany <stefany_madrid@unitec.edu>
2026-09-13 22:06:35 -0600

    -se agrego switch para alternar entre tema claro y oscuro

--- QueLLevo/src/screens/ProfileScreen.tsx
+++ QueLLevo/src/screens/ProfileScreen.tsx
@@ -1,16 +1,19 @@
 import { useNavigation } from "@react-navigation/native";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Switch ,Text, View } from "react-native";
 import CustomButton from "../components/CustomButton";
-import { colors } from "../constants/colors";
 import { cerrarSesion } from "../store/slices/userSlice";
+import { alternarTema } from "../store/slices/temaSlice";
+import { useTema } from "../store/useTema";
 
 
 export default function ProfileScreen() {
   const navigation = useNavigation<any>();
   const dispatch = useAppDispatch();
   const usuario = useAppSelector((state) => state.usuario.datos);
+  const { modo, colores} = useTema();
+  const styles = getStyles(colores);
 
   return (
     <View style={styles.contenedor}>
@@ -18,6 +21,17 @@ export default function ProfileScreen() {
       <Text style={styles.titulo}>{usuario?.nombre ?? "Mi perfil"}</Text>
       <Text style={styles.texto}>{usuario?.email}</Text>
       <Text style={styles.texto}>{usuario?.telefono}</Text>
+
+      <View style={styles.filaTema}>
+        <Text style={styles.textoTema}>Tema oscuro</Text>
+        <Switch
+          value={modo === "oscuro"}
+          onValueChange={() => {
+            dispatch(alternarTema());
+          }}
+          trackColor={{ false: colores.border, true: colores.primary }}
+        />
+      </View>
       
 
       <CustomButton
@@ -33,9 +47,23 @@ export default function ProfileScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  contenedor: { flex: 1, backgroundColor: colors.background, alignItems: "center", paddingTop: 48, paddingHorizontal: 24 },
+function getStyles(colores: ReturnType<typeof useTema>["colores"]){
+ return StyleSheet.create({
+  contenedor: { flex: 1, backgroundColor: colores.background, alignItems: "center", paddingTop: 48, paddingHorizontal: 24 },
   avatar: { width: 96, height: 96, marginBottom: 16, borderRadius: 20 },
-  titulo: { fontSize: 22, fontWeight: "800", color: colors.text },
-  texto: { fontSize: 14, color: colors.textMuted, textAlign: "center", marginTop: 8 },
+  titulo: { fontSize: 22, fontWeight: "800", color: colores.text },
+  texto: { fontSize: 14, color: colores.textMuted, textAlign: "center", marginTop: 4},
+  filaTema: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+    width: "100%",
+    backgroundColor: colores.card,
+    borderRadius: 12,
+    padding: 16,
+    marginTop:24,
+  },
+  textoTema: { fontSize: 15, fontWeight: "600", color: colores.text},
 });
+
+}
