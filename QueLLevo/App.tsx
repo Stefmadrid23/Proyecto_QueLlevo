63d50d3c874c19541b67c01d373b5825ee9814ef
Stefany <stefany_madrid@unitec.edu>
2026-09-13 22:05:57 -0600

    - carga el tema guardado al iniciar y ajustar StatusBar segun el modo

--- QueLLevo/App.tsx
+++ QueLLevo/App.tsx
@@ -3,26 +3,33 @@ import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Store } from '@reduxjs/toolkit';
-import { useAppDispatch } from './src/store/hooks';
+import { useAppDispatch, useAppSelector } from './src/store/hooks';
 import { cargarListasDesdeStorage } from './src/store/slices/listaSlice';
 import RootNavigator from './src/navigation/RootNavigator';
 import { store } from './src/store/store';
+import { cargarTemaDesdeStorage } from './src/store/slices/temaSlice';
 
 function InicioApp (){
   const dispatch = useAppDispatch();
+  const modo = useAppSelector((state) => state.tema.modo);
 
   useEffect(() => {
     dispatch(cargarListasDesdeStorage());
+    dispatch(cargarTemaDesdeStorage());
   }, [dispatch]);
 
-  return <RootNavigator />;
+  return (
+  <>
+  <StatusBar style={modo === "oscuro" ? "light": "dark"} />
+  <RootNavigator />
+  </>
+  );
 }
 
 export default function App() {
   return (
     <Provider store={store}>
     <SafeAreaProvider>
-      <StatusBar style= "dark" />
       <InicioApp />
     </SafeAreaProvider>
     </Provider>
